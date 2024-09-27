cb495897c29cbfb61d0f5a4cb055cc59cdb2cd52
DarsaniTS07 <darsani.ec23@bitsathy.ac.in>
2024-09-27 10:21:00 +0530

    added image

--- src/pages/Survey/SurveyPage.jsx
+++ src/pages/Survey/SurveyPage.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 import './SurveyPage.css';
+import surveypage1 from "/src/assets/SurveyPage1.png";
+import surveypage2 from "/src/assets/SurveyPage2.png";
+import surveypage3 from "/src/assets/surveypage3.png";
+import surveypage4 from "/src/assets/surveypage4.png";
+import surveypage5 from "/src/assets/surveypage5.png";
+import surveypage6 from "/src/assets/surveypage6.png";
 
 const SurveyPage = ()=>{
     const [score,setScore] = useState(0);
@@ -16,6 +22,9 @@ const SurveyPage = ()=>{
     return(
         <div>
             <div className='container'>
+                <div>
+                    <img src='/src/assets/SurveyPage1.png' alt='surveypage1'/>
+                </div>
                 <div className='inner-container '>
                     <div className='imageContainer'>
                         {
@@ -29,7 +38,6 @@ const SurveyPage = ()=>{
                     </div> 
 
                 </div>
-
             </div>
         </div>
     )
