48340ce1b83a3d0f787f09d042eff13f88612912
Sabareesh22 <sabareesh.cs22@bitsathy.ac.in>
2024-09-27 21:24:58 +0530

    completed survey page

--- src/pages/Survey/SurveyPage.jsx
+++ src/pages/Survey/SurveyPage.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import "./SurveyPage.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./SurveyPage.css";
 import surveypage1 from "/src/assets/SurveyPage1.png";
 import surveypage2 from "/src/assets/SurveyPage2.png";
@@ -8,11 +6,21 @@ import surveypage3 from "/src/assets/surveypage3.png";
 import surveypage4 from "/src/assets/surveypage4.png";
 import surveypage5 from "/src/assets/surveypage5.png";
 import surveypage6 from "/src/assets/surveypage6.png";
-import car from "/src/assets/car.png";
-import motor from "/src/assets/Motor Scooter.png";
-import bicycle from "/src/assets/Bicycle.png"
+import bicycle from "/src/assets/Bicycle.png";
+import {
+  CircularProgressbarWithChildren,
+  buildStyles,
+} from "react-circular-progressbar";
+import Card from "../../components/circularProgressBarWithLabel/card/Card";
+import Button from "../../components/circularProgressBarWithLabel/card/button/Button";
+import Vehicles from "./vehicles/PromptContainer";
+import PromptContainer from "./vehicles/PromptContainer";
 const SurveyPage = () => {
   const [score, setScore] = useState(0);
+  const [currStep, setCurrStep] = useState(1);
+  const [prevStep,setPrevStep] = useState(1);
+  const [isLast,setIsLast] = useState(false);
+  const totalSteps = 4;
   const [backgroundImages, setBackgroundImages] = useState([
     {
       score: 0,
@@ -39,48 +47,61 @@ const SurveyPage = () => {
       img_url: surveypage6,
     },
   ]);
-
-  const handleNext = () => {
-
-  }
   return (
-    <>
-    <div>
-      <div className="container">
-        <div className="inner-container ">
-          <div className="imageContainer">
-            {
-              <img
-                src={
-                  backgroundImages?.find((data) => {
-                    return data.score === score;
-                  }).img_url
-                }
-              />
-            }
-          </div>
-          <div className="question-container">
-            <div className="question">
-              Choose the vehicle you use for commuting?
-            </div>
-            <div className="options">
-              <div className="bicylce">
-                <img src={bicycle} alt="bicycle"/>
-                <div style={{paddingTop:'7px'}}>Cycle</div>
-              </div>
-              <div className="two-wheeler">
-              <img src={motor} alt="bicycle"/>
-              <div style={{paddingTop:'7px'}}>Two-wheeler</div>
-              </div>
-              <div className="car">
-              <img src={car} alt="bicycle"/>
-              <div style={{paddingTop:'7px'}}>Car</div>
-              </div>
-              </div>
-            </div>
-            <div className="next" onClick={handleNext}>Next</div>
+    <div className="container">
+      <div className="inner-container ">
+        <div className="imageContainer">
+          {
+            <img
+              src={
+                backgroundImages?.find((data) => {
+                  return data.score === score;
+                }).img_url
+              }
+            />
+          }
+        </div>
+      </div>
+      <div className="questionContainer">
+        <div className="progressBarContainer">
+          <div className="progressBar">
+            <CircularProgressbarWithChildren
+              maxValue={totalSteps}
+              value={Math.floor(currStep)}
+              styles={buildStyles({
+                pathTransitionDuration: 0.5,
+                strokeWidth: 10,
+                rotation: 0,
+                trailColor: "#FFF4E4",
+                pathColor: "#FEA062",
+              })}
+            >
+              <div className="stepDisplay">{`${Math.floor(currStep)}/${totalSteps}`}</div>
+            </CircularProgressbarWithChildren>
           </div>
         </div>
+       <div className="stepPageContainer">
+       <PromptContainer setIsLast={setIsLast} setCurrStep={setCurrStep} currStep={currStep}  />
+       </div>
+        <div className="buttonsContainer">
+        { !isLast && currStep<totalSteps+0.9 &&
+          <Button 
+          onClick={()=>{
+            setPrevStep(currStep)
+            setCurrStep((prev)=>(prev+0.1))
+          }}
+          content={"Next"} />
+          }
+        {  !isLast &&  currStep>1 && currStep<totalSteps+1 &&
+          <Button
+          onClick={()=>{
+              setCurrStep(prevStep)
+          }}
+          content={"Back"} />}
+       { isLast &&
+        <Button content={"Calculate Carbon Footprint"}/>}
+        </div>
+        
       </div>
     
     </>
