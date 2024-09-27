85d769ec0f26af6f2a1f82ea23ac8185143e1073
Sabareesh22 <sabareesh.cs22@bitsathy.ac.in>
2024-09-27 09:23:19 +0530

    test

--- src/pages/Survey/SurveyPage.jsx
+++ src/pages/Survey/SurveyPage.jsx
@@ -3,7 +3,7 @@ import './SurveyPage.css';
 const SurveyPage = ()=>{
     return(
         <div>
-            <h1>{"Page 2 --> Calculations Page"}</h1>
+            <h1>{"Page 2 --> "}</h1>
         </div>
     )
 }
