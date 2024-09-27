f13e5c4783055309f542c2cb5d6d2181fe70a530
kabilECE <kabil.7376231ec174@gmail.com>
2024-09-27 09:32:18 +0530

    fix: adding template

--- src/pages/KarmaReport/ReportPage.jsx
+++ src/pages/KarmaReport/ReportPage.jsx
@@ -1,11 +1,17 @@
-import './ReportPage.css';
+import "./ReportPage.css";
+import bdImg from "../../assets/Mask Group 112.svg";
+import "../../assets/Fonts/css/excon.css";
+const ReportPage = () => {
+	return (
+		<div className="parent" style={{ backgroundImage: `url(${bdImg})` }}>
+			<div className="head">
+				<h3>Summary</h3>
+			</div>
+			<div className="body">
 
-const ReportPage = ()=>{
-    return(
-        <div>
-            <h1>{"Page 3 --> Report Page"}</h1>
-        </div>
-    )
-}
+			</div>
+		</div>
+	);
+};
 
 export default ReportPage;
