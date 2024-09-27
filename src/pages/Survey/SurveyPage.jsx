2409e1d4a9c843766698060a40f9a49d376e362b
DarsaniTS07 <darsani.ec23@bitsathy.ac.in>
2024-09-27 10:21:00 +0530

    added image

--- src/pages/Survey/SurveyPage.jsx
+++ src/pages/Survey/SurveyPage.jsx
@@ -1,16 +1,21 @@
 import './SurveyPage.css';
+import surveypage1 from "/src/assets/SurveyPage1.png";
+import surveypage2 from "/src/assets/SurveyPage2.png";
+import surveypage3 from "/src/assets/surveypage3.png";
+import surveypage4 from "/src/assets/surveypage4.png";
+import surveypage5 from "/src/assets/surveypage5.png";
+import surveypage6 from "/src/assets/surveypage6.png";
 
 const SurveyPage = ()=>{
     return(
         <div>
             <div className='container'>
                 <div>
-                    <img src='/src/assets/SurveyPage1.png'/>
+                    <img src='/src/assets/SurveyPage1.png' alt='surveypage1'/>
                 </div>
                 <div className='inner-container '>
                     <div></div>
                 </div>
-
             </div>
         </div>
     )
