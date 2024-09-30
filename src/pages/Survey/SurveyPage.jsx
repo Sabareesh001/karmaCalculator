3625874a8ac120c4b4a347a8a8dee54287ac0d4e
kabilECE <kabil.7376231ec174@gmail.com>
2024-09-30 11:24:40 +0530

    resolved

--- src/pages/Survey/SurveyPage.jsx
+++ src/pages/Survey/SurveyPage.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react';
 import './SurveyPage.css';
 
 const SurveyPage = ()=>{
+    const [score,setScore] = useState(0);
+    const [backgroundImages,setBackgroundImages] = useState([
+        {
+            score:0,
+            img_url:'/src/assets/SurveyPage1.png'
+        },
+        {
+            score:10,
+
+        }
+    ])
     return(
         <div>
             <div className='container'>
-                <div>
-                    <img src='/src/assets/SurveyPage1.png'/>
-                </div>
-                <div className='inner-container'>
-                    
+                <div className='inner-container '>
+                    <div className='imageContainer'>
+                        {
+                           
+                            <img src={ backgroundImages?.find((data)=>{
+                                return(
+                                    data.score === score
+                                )
+                            }).img_url}/>
+                        }
+                    </div> 
+
                 </div>
 
             </div>
